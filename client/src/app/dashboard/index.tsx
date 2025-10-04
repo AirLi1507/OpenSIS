fa1b894503814489c2174be62c01f26f27d24c67
AirLi1507 <fung161122@gmail.com>
2025-10-04 22:39:03 +0800

    fix: removing dashboard provider in dashboard layout

--- client/src/app/dashboard/index.tsx
+++ client/src/app/dashboard/index.tsx
@@ -1,21 +1,18 @@
 import { Outlet } from "react-router"
 import Sidebar from "../../components/dashboard/sidebar"
 import UserProvider from "../../providers/user"
-import DashboardProvider from "../../providers/dashboard"
 import "./index.css"
 
 const Dashboard = () => {
   return (
-    <DashboardProvider>
-      <UserProvider>
-        <div id="dashboard" className="sidebar">
-          <Sidebar />
-          <main>
-            <Outlet />
-          </main>
-        </div>
-      </UserProvider>
-    </DashboardProvider>
+    <UserProvider>
+      <div id="dashboard" className="sidebar">
+        <Sidebar />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </UserProvider>
   )
 }
 
